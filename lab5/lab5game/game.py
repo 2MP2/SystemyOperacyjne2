463dac8264923c7ea8e2334f5756c4e005775b61
mikpas138_dpp_2023 <mikpas138_dpp_2023@e-science.pl>
2023-05-29 15:51:09 +0200

    changes to 5x5

--- lab5/lab5game/game.py
+++ lab5/lab5game/game.py
@@ -16,6 +16,7 @@ current_player = "X"
 window = tk.Tk()
 window.title("Kółko i krzyżyk")
 
+
 # Funkcja oznaczająca wybrane pole
 def mark_square(row, col):
     global current_player
@@ -27,35 +28,37 @@ def mark_square(row, col):
 
         if check_winner(current_player):
             messagebox.showinfo("Koniec gry", "Gracz {} wygrał!".format(current_player))
-            reset_game()
+            new_board()
         else:
             if current_player == "X":
                 current_player = "O"
             else:
                 current_player = "X"
 
+
 # Funkcja sprawdzająca warunki zwycięstwa
 def check_winner(player):
     # Sprawdzenie wierszy
     for row in board:
-        if row.count(player) == 3:
+        if row.count(player) == 5:
             return True
 
     # Sprawdzenie kolumn
-    for col in range(3):
-        if [board[row][col] for row in range(3)].count(player) == 3:
+    for col in range(5):
+        if [board[row][col] for row in range(5)].count(player) == 5:
             return True
 
     # Sprawdzenie przekątnych
-    if board[0][0] == board[1][1] == board[2][2] == player:
+    if board[0][0] == board[1][1] == board[2][2] == board[3][3] == board[4][4] == player:
         return True
-    if board[0][2] == board[1][1] == board[2][0] == player:
+    if board[0][4] == board[1][3] == board[2][2] == board[3][1] == board[4][0] == player:
         return True
 
     return False
 
+
 # Funkcja resetująca grę
-def reset_game():
+def new_board():
     global current_player
     board.clear()
     board.extend([[" ", " ", " ", " ", " "],
@@ -67,6 +70,7 @@ def reset_game():
     for row in range(5):
         for col in range(5):
             buttons[row][col].config(text=" ")
+
 
 # Tworzenie przycisków na planszy
 buttons = []
